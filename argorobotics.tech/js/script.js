a08a0ede018a593bdcbbbbef627873b4cf389af1
alexchia165 <s161129@scusd.net>
2024-02-10 21:18:10 -0800

    Update script.js

---
+++ argorobotics.tech/js/script.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("my-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Automatic slideshow
+setInterval(() => {
+  plusSlides(1);
+}, 5000); // Change slide every 5 seconds (adjust as needed)
