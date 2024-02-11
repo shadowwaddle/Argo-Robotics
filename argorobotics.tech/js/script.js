6667661271dcf5aa9e8c14becbb8f59a849ba0d3
alexchia165 <s161129@scusd.net>
2024-02-10 22:03:38 -0800

    Update script.js

--- argorobotics.tech/js/script.js
+++ argorobotics.tech/js/script.js
@@ -1,27 +1,26 @@
-function plusSlides(n, slideshowId) {
-    showSlides(slideIndex[slideshowId] += n, slideshowId);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-  
-function showSlides(n, slideshowId) {
-    let i;
-    const slides = document.querySelectorAll(`.${slideshowId} .my-slide`);
-    if (n > slides.length) {
-      slideIndex[slideshowId] = 1;
-    }
-    if (n < 1) {
-      slideIndex[slideshowId] = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex[slideshowId] - 1].style.display = "block";
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("my-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
-  
+
 // Automatic slideshow
 setInterval(() => {
-    plusSlides(1, 'slideshow-1');
-    plusSlides(1, 'slideshow-2');
-    plusSlides(1, 'slideshow-3');
-    // Add more slideshow instances as needed
+  plusSlides(1);
 }, 5000); // Change slide every 5 seconds (adjust as needed)
-  
