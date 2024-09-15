871fd909358e57be98cc65a3b992d3fe4f46cd7a
sharvilkulkarni <s167314@scusd.net>
2024-09-15 16:25:42 -0700

    Enabled the opMode

--- FtcRobotController-master/FtcRobotController-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
+++ FtcRobotController-master/FtcRobotController-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
@@ -65,6 +65,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
 //@Disabled
+//Random comment to test pushing, please delete later.
 public class BasicOmniOpMode_Linear extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
