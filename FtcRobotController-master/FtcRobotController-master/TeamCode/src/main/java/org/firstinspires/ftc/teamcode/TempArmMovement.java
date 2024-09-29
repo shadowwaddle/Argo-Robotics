48778c1771ec1698f75f7e7030aaa8a316a10dd2
sharvilkulkarni <s167314@scusd.net>
2024-09-29 16:08:07 -0700

    Created a test opMode to see if we can make the arm go up. The goal is to delete this opMode and move all the code into the main movement file.

---
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempArmMovement.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//Temporary OpMode to test the arm angle motion. Will be moved into the main opmode after testing.
+@TeleOp(name  = "Arm Movement Testing",
+        group = "Linear OpMode")
+public class TempArmMovement extends LinearOpMode {
+
+    //Declaring time counter variable, Arm motor object
+    private ElapsedTime runTime = new ElapsedTime();
+    private DcMotor armMotor    = null;
+
+    @Override
+    public void runOpMode() {
+
+        //Mapping the motor
+        armMotor  = hardwareMap.get(DcMotor.class, "armMotor");
+
+        //While OpMode is active, checks each button to determine movement direction
+        while (opModeIsActive()) {
+
+            if (gamepad2.a) {
+                armMotor.setPower(0.1);
+            } else if (gamepad2.b) {
+                armMotor.setPower(-0.1);
+            } else {
+                armMotor.setPower(0);
+            }
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+        }
+
+    }
+}
